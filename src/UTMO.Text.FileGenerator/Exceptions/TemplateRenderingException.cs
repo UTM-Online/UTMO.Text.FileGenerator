3f9d6129896aa937cb4b4330ca7b4a24416616fc
Josh Irwin <joshua@irwin.ws>
2023-11-24 14:36:58 -0800

    Updating TemplateRenderingException.cs

--- src/UTMO.Text.FileGenerator/Exceptions/TemplateRenderingException.cs
+++ src/UTMO.Text.FileGenerator/Exceptions/TemplateRenderingException.cs
@@ -8,17 +8,11 @@ public class TemplateRenderingException : ApplicationException
 {
     public TemplateRenderingException(string message, Dictionary<string,object> model, string outputPath) : base($"A exception occured rending template {model["TemplatePath"]} of type {model["ResourceTypeName"]} with name {model["ResourceName"]} to {outputPath} with message {message}")
     {
-        this.TemplateName = model["TemplatePath"].Cast<string>();
+        this.TemplateName = model["TemplateName"].Cast<string>();
         this.OutputFileName = outputPath;
-        this.ResourceTypeName = model["ResourceTypeName"].Cast<string>();
-        this.ResourceName = model["ResourceName"].Cast<string>();
     }
     
     public string TemplateName { get; set; }
     
     public string OutputFileName { get; set; }
-    
-    public string ResourceTypeName { get; set; }
-    
-    public string ResourceName { get; set; }
 }
