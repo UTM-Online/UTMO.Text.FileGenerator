aad5c266eff5cc14526bf0160e8b257ed9f1f957
Josh Irwin <joirwi@microsoft.com>
2023-12-29 13:33:38 -0800

    Adding methods to enable combining of manifests.

---
+++ src/UTMO.Text.FileGenerator/Extensions/ManifestExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+
+namespace UTMO.Text.FileGenerator.Extensions;
+
+public static class ManifestExtensions
+{
+    public static dynamic CombineManifests(this object object1, object object2)
+    {
+        IDictionary<string, object> dictionary1 = GetKeyValueMap(object1);
+        IDictionary<string, object> dictionary2 = GetKeyValueMap(object2);
+
+        var result = new ExpandoObject();
+
+        var d = result as IDictionary<string, object>;
+        foreach (var pair in dictionary1.Concat(dictionary2))
+        {
+            d[pair.Key] = pair.Value;
+        }
+
+        return result;
+    }
+    
+    private static IDictionary<string, object> GetKeyValueMap(object? values)
+    {
+        if (values == null)
+        {
+            return new Dictionary<string, object>();
+        }
+
+        var map = values as IDictionary<string, object>;
+        if (map == null)
+        {
+            return map;
+        }
+
+        map = new Dictionary<string, object>();
+        foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(values))
+        {
+            map.Add(descriptor.Name, descriptor.GetValue(values) ?? throw new InvalidOperationException());
+        }
+
+        return map;
+    }
+}
